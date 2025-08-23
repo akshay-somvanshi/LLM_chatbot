cea458dd0a10942fbc176d07f00eaa24b73136d7
akshay-somvanshi <akshay.somvanshi24@imperial.ac.uk>
2025-08-23 16:14:46 +0200

    Updated chatbot with personalized user experience

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from langchain_text_splitters import RecursiveJsonSplitter
 from langchain_chroma import Chroma
 from sentence_transformers import SentenceTransformer
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.schema import Document
 import json
 import os
+import time
 
 # Load From .env File
 API_Key = os.environ.get("API_Key")
@@ -21,7 +23,7 @@ local_llm = ChatOpenAI(
 )
 
 cloud_llm = ChatOpenAI(
-    model = "openai/gpt-oss-20b:free",
+    model = "deepseek/deepseek-r1-0528-qwen3-8b:free",
     api_key = API_Key,
     base_url = Remote_base_url
 )
@@ -35,7 +37,7 @@ def retrieve_data(docs):
     for file in docs:   
         with open(file, "r") as f:
             data = json.loads(f.read())
-            splitter = RecursiveJsonSplitter(max_chunk_size=300)
+            splitter = RecursiveJsonSplitter(max_chunk_size=900)
             documents = splitter.create_documents(texts=data, convert_lists=True)
             all_documents.extend(documents)  # Add to collection
     return all_documents
@@ -60,9 +62,6 @@ vector_retriever = vectordb.as_retriever(search_kwargs={"k": 3})
 
 ############################################
 
-# Set the UI title
-st.title("LLM ChatBot")
-
 cloud_box = st.checkbox(
     "Use Cloud LLM",
     value = False
@@ -75,67 +74,99 @@ st.session_state.setdefault(
     []
 )
 
-# Iterate through the messages and print them on the console
-for msg in st.session_state["messages"]:
-    # Print based on role and content
-    with st.chat_message(msg["role"]):
-        st.write(msg["content"])
+st.session_state.setdefault(
+    "user",
+    # No user empty string initialisation
+    ""
+)
 
-prompt = st.chat_input("Type message here")
+# Set the UI title based on whether user name is known 
+if st.session_state["user"] == "":
+    st.title("Enter your full name")
+    prompt = st.chat_input("Enter your name here")
+    if prompt:
+        # Store the user name in the session state so we can use it for data retrieval
+        st.session_state["user"] = prompt
+        # Refresh to show new interface
+        st.rerun()
+else:
+    st.title(f"Hello {st.session_state["user"]}! How can I help?")
 
-if prompt:
-
-    # Searching the database with the user prompt
-    relevant_info = vector_retriever.get_relevant_documents(prompt)
-
-    # Extract the text content from the search result
-    retrieved_data = [data.page_content for data in relevant_info]
-
-    # System message for priming AI behavior.
-    message = [{"role": "system", "content": "You are a helpful assistant.When appropriate, provide planning-oriented responses with actionable steps \
-                with key insights."}]
-    
-    if(retrieve_data):
-        content_data = "\n".join(retrieved_data)
-        message.append({
-            "role": "system",
-            "content" : f"Relevant user information:\n{content_data}"
-        })
-
-    # Store the user messages in a dictionary
-    st.session_state["messages"].append(
-        {
-            "role": "user",
-            "content": prompt
-        }
-    )
-
-    # Pre-built user role
-    with st.chat_message("user"):
-        st.write(prompt)
-
-    # For the chatbot to remember conversations
+    # Iterate through the messages and print them on the console
     for msg in st.session_state["messages"]:
-        message.append({
-            "role": msg["role"],
-            "content": msg["content"]
-        })
+        # Print based on role and content
+        with st.chat_message(msg["role"]):
+            st.write(msg["content"])
 
-    if cloud_box:
-        llm = cloud_llm
-    else:
-        llm = local_llm
+    prompt = st.chat_input("Type message here")
 
-    response = llm.invoke(message)
+    def stream_data(response):
+        for word in response.split(" "):
+            yield word + " "
+            time.sleep(0.02)
 
-    # Store the LLM messages in a dictionary
-    st.session_state["messages"].append(
-        {
-            "role": "assistant",
-            "content": response.content
-        }
-    )
+    if prompt:
+        # Filter to only include current user's data
+        current_user = st.session_state["user"]
 
-    # Pre-built assistant role
-    with st.chat_message("assistant"):
-        st.write(response.content)
+        # Append the query with the user information to retrieve the correct data
+        search_query = f"User: {st.session_state['user']}. Question: {prompt}"
+
+        # Searching the database with the user prompt
+        relevant_info = vector_retriever.get_relevant_documents(search_query)
+
+        # Extract the text content from the search result
+        retrieved_data = [data.page_content for data in relevant_info]
+
+        # System message for priming AI behavior.
+        message = [{"role": "system", "content": f"You are a helpful assistant. When appropriate, provide planning-oriented responses with actionable steps \
+                    with key insights."}]
+        
+        if retrieved_data:
+            st.sidebar.write("Retrieved Context:")
+            for i, data in enumerate(retrieved_data, 1):
+                st.sidebar.write(f"{i}. {data}")
+            content_data = "\n".join(retrieved_data)
+            message.append({
+                "role": "system",
+                "content" : f"\n Relevant user information:\n{content_data}"
+            })
+
+        # Store the user messages in a dictionary
+        st.session_state["messages"].append(
+            {
+                "role": "user",
+                "content": prompt
+            }
+        )
+
+        # Pre-built user role
+        with st.chat_message("user"):
+            st.write(prompt)
+
+        # For the chatbot to remember conversations
+        for msg in st.session_state["messages"]:
+            message.append({
+                "role": msg["role"],
+                "content": msg["content"]
+            })
+
+        if cloud_box:
+            llm = cloud_llm
+        else:
+            llm = local_llm
+
+        response = llm.invoke(message)
+
+        # Store the LLM messages in a dictionary
+        st.session_state["messages"].append(
+            {
+                "role": "assistant",
+                "content": response.content
+            }
+        )
+
+        # Pre-built assistant role
+        with st.chat_message("assistant"):
+            st.write_stream(stream_data(response.content))
+            # st.write(response.content)
